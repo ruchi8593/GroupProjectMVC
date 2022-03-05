a23bd4d6e72d3c4eea8fd181524dd18f75e12815
Vivek-Kathiriya <96758086+Vivek-Kathiriya@users.noreply.github.com>
2022-03-05 14:02:43 -0500

    Mvc dal final (#17)
    * Final commit
    
    * Final Commit
    
    Co-authored-by: VivekKathiriya <kathiriyavivek2611@gmail.comgit config --global user.name Vivek-Kathiriya>

---
+++ MVC_Identity_DataLayer/Controllers/PersonController.cs
@@ -0,0 +1,156 @@
+﻿using MVC_Identity_DataLayer.Models;
+using MVC_Identity_DataLayer_Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static MVC_Identity_DataLayer_Data.Person;
+
+namespace MVC_Identity_DataLayer.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            Person dal = new Person();
+
+            var persons = dal.GetAllPerson();
+
+            List<PersonModel> personList = new List<PersonModel>();
+
+            foreach (var person in persons)
+            {
+                PersonModel person1 = new PersonModel();
+
+                person1.PersonID = person.PersonID;
+                person1.Age = person.Age;
+                person1.AddressID = person.AddressID;
+                person1.Gender = person.Gender;
+                person1.FirstName = person.FirstName;
+                person1.LastName = person.LastName;
+                person1.EmailID = person.EmailID;
+
+                personList.Add(person1);
+
+            }
+            return View(personList);
+        }
+
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    PersonDetails person = new PersonDetails();
+
+                    person.FirstName = collection["FirstName"];
+                    person.LastName = collection["LastName"];
+                    person.Age = Convert.ToInt32(collection["Age"]);
+                    person.Gender = collection["Gender"];
+                    person.EmailID = collection["EmailID"];
+                    person.AddressID = Convert.ToInt32(collection["AddressID"]);
+
+
+                    Person dal = new Person();
+
+                    dal.InsertPerson(person);
+
+                    RedirectToAction("Index");
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Person dal = new Person();
+
+            var personDetails = dal.GetPerson(id);
+
+            PersonModel person = new PersonModel()
+            {
+                PersonID = personDetails.PersonID,
+                FirstName = personDetails.FirstName,
+                LastName = personDetails.LastName,
+                Age = Convert.ToInt32(personDetails.Age),
+                Gender = personDetails.Gender,
+                EmailID = personDetails.EmailID,
+                AddressID = personDetails.AddressID
+            };
+
+            return View(person);
+        }
+
+        // POST: Person/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                PersonDetails person = new PersonDetails();
+
+                person.PersonID = Convert.ToInt32(id);
+                person.FirstName = collection["FirstName"];
+                person.LastName = collection["LastName"];
+                person.Age = Convert.ToInt32(collection["Age"]);
+                person.Gender = collection["Gender"];
+                person.EmailID = collection["EmailID"];
+                person.AddressID = Convert.ToInt32(collection["AddressID"]);
+
+                Person dal = new Person();
+
+                dal.UpdatePerson(person);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Delete/5
+        [HttpDelete]
+        public ActionResult DeletePerson(int PersonID)
+        {
+            Person dal = new Person();
+            dal.DeletePerson(PersonID);
+            return null;
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+
+}
+
---
+++ MVC_Identity_DataLayer/Models/PersonModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Identity_DataLayer.Models
+{
+    public class PersonModel
+    {
+        [Display(Name = "ID")]
+        public int PersonID { get; set; }
+
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+
+        [Display(Name = "Email")]
+        public string EmailID { get; set; }
+
+        public string Gender { get; set; }
+
+
+        [Display(Name = "Address ID")]
+        public int AddressID { get; set; }
+    }
+
+}
