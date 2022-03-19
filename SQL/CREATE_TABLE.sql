f5d47de7378d6f2f0783c1a07e78d98d5d194d80
Ruchi Trivedi <ruchitrivedi99995@gmail.com>
2022-03-18 23:31:39 -0400

    Added UserInfo table and made other changes

--- SQL/CREATE_TABLE.sql
+++ SQL/CREATE_TABLE.sql
@@ -3,10 +3,23 @@
 CREATE DATABASE MVCDatabase 
 
 USE MVCDatabase
+GO
 
+IF OBJECT_ID(N'dbo.Province', N'U') IS NOT NULL
+DROP TABLE [dbo].[Province]
+GO
+
+IF OBJECT_ID(N'dbo.Address', N'U') IS NOT NULL
+DROP TABLE [dbo].[Address]
+GO
+IF OBJECT_ID(N'dbo.Person', N'U') IS NOT NULL
+DROP TABLE [dbo].[Person]
+GO
+IF OBJECT_ID(N'dbo.UserInfo', N'U') IS NOT NULL  
+   DROP TABLE [dbo].[UserInfo]
+GO
 
 ---create Province table
-
 CREATE TABLE Province(
 	ProvinceID				INT	PRIMARY KEY IDENTITY(1,1),
 	ProvinceName			VARCHAR(25),
@@ -15,7 +28,7 @@ CREATE TABLE Province(
 
 	
 ---create Address table
-	
+
 CREATE TABLE Address(
 	AddressID		INT PRIMARY KEY IDENTITY(1,1) ,
 	Address			VARCHAR(25) NOT NULL,
@@ -39,6 +52,16 @@ CREATE TABLE Person
 	AddressID	INT FOREIGN KEY REFERENCES Address(AddressID)
 	);
 
+
+--- User table
+
+CREATE TABLE UserInfo
+(
+	Id	  INT PRIMARY KEY IDENTITY(1,1),
+	Username	VARCHAR(25) NOT NULL,
+	Password	VARCHAR(25) NOT NULL,
+	Role	    VARCHAR(25) NOT NULL
+);	
 
 ---Province Table Value
 
@@ -122,6 +145,58 @@ VALUES ( 'Nirav', 'Patel', 28, 'niravpatel@gmail.com', 'Male' , 5 );
 
 INSERT INTO MVCDatabase.dbo.Person  (  FirstName, LastName, Age, EmailID, Gender, AddressID)
 VALUES ( 'Harsh', 'Patel', 25, 'harshpatel@gmail.com', 'Male' , 6 );
+
+
+---user table value
+
+
+INSERT INTO MVCDatabase.dbo.UserInfo(Username, Password, Role)
+VALUES ( 'Vivek', 'Test', 'Admin');
+INSERT INTO MVCDatabase.dbo.UserInfo(Username, Password, Role)
+VALUES ( 'Ruchi', 'Test1', 'Admin');
+INSERT INTO MVCDatabase.dbo.UserInfo(Username, Password, Role)
+VALUES ( 'Nirav', 'Test2', 'Admin');
+INSERT INTO MVCDatabase.dbo.UserInfo(Username, Password, Role)
+VALUES ( 'Urvashi', 'Test3', 'User');
+INSERT INTO MVCDatabase.dbo.UserInfo(Username, Password, Role)
+VALUES ( 'Harsh', 'Test4', 'Reviewer');
+INSERT INTO MVCDatabase.dbo.UserInfo(Username, Password, Role)
+VALUES ( 'Niruxa', 'Test5', 'Tester');
+
+
+-- get user info
+
+DROP PROCEDURE IF EXISTS SP_get_User
+GO
+CREATE PROCEDURE SP_get_User  
+@Username VARCHAR(25),
+@Password	VARCHAR(25)
+AS  
+BEGIN  
+SELECT Username, Password, Role  
+FROM UserInfo   
+WHERE (Username = @Username AND Password = @Password);
+END  
+GO
+
+-- stored procedure for add userinfo
+
+DROP PROCEDURE IF EXISTS SP_Create_User
+GO
+CREATE PROCEDURE SP_Create_User  
+@Username VARCHAR(25),
+@Password	VARCHAR(25),
+@Role VARCHAR(25) = "User"
+AS  
+BEGIN  
+INSERT INTO UserInfo(  
+Username,  
+Password, 
+Role )
+VALUES (  
+@Username,  
+@Password, @Role )
+END
 
 
 /*DROP TABLE dbo.Address
